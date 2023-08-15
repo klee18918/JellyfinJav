6a1ab2e3be087e3436d427cfa74d44b76ad5a173
klee18918 <klee18918@gmail.com>
2023-08-15 15:53:45 -0700

    update release URL

--- package.py
+++ package.py
@@ -46,7 +46,7 @@ entry = {
     "checksum": md5(open(f"release/jellyfinjav_{version}.zip", "rb").read()).hexdigest(),
     "changelog": "",
     "targetAbi": f"{targetAbi}.0",
-    "sourceUrl": f"https://github.com/imaginary-upside/JellyfinJav/releases/download/{version}/jellyfinjav_{version}.zip",
+    "sourceUrl": f"https://github.com/klee18918/JellyfinJav/releases/download/{version}/jellyfinjav_{version}.zip",
     "timestamp": timestamp,
     "version": version
 }
